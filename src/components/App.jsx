fd1380e91e15ae7e6e106175b2c792ef6d7f4237
Ladao <kennedy.ladao@accenture.com>
2023-10-05 15:34:35 +0800

    API Integration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
-
 function App() {
   return (
     <div className="App">
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ItemForm from './ItemForm'
 import Item from './Item'
 
@@ -7,6 +7,30 @@ function ShoppingList() {
     const [selectedCategory, setSelectedCategory] = useState("All");
     const [items, setItems] = useState([]);
 
+    useEffect(() => {
+      fetch('http://localhost:4000/items')
+      .then((r) => r.json())
+      .then((items) => setItems(items))
+    }, [])
+    
+    function handleAddItem(newItem) {
+      setItems([...items, newItem])
+    }
+
+    function handleDeleteItem(deletedItem) {
+      const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+      setItems(updatedItems);
+    }
+    function handleUpdateItem(updatedIem) {
+      const updatedItems = items.map((item) => {
+        if(item.id === updatedIem.id){
+          return updatedIem
+        } else {
+          return item
+        }
+      })
+      setItems(updatedItems)
+    }
     const itemsToDisplay = items.filter((item) => {
         if (selectedCategory === "All") return true;
     
@@ -14,10 +38,16 @@ function ShoppingList() {
       });
     return (
         <div className="ShoppingList">
-        <ItemForm />
+        <ItemForm onAddItem={handleAddItem}/>
+
         <ul className="Items">
           {itemsToDisplay.map((item) => (
-            <Item key={item.id} item={item} />
+            <Item 
+              key={item.id} 
+              item={item} 
+              onUpdatedItem={handleUpdateItem}
+              onDeleteItem={handleDeleteItem}
+              />
           ))}
         </ul>
       </div>
